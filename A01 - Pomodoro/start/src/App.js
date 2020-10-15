3cfecf57c2fc9310ef3af6cf3444d9246ec120ab
Jacob Bremer <bremerjp@gmail.com>
2020-10-15 08:30:14 -0400

    completed app

--- A01 - Pomodoro/start/src/App.js
+++ A01 - Pomodoro/start/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function padTime(time) {
@@ -7,15 +7,34 @@ function padTime(time) {
 
 export default function App() {
   const [title, setTitle] = useState('Pomodoro Timer');
-  const [timeLeft, setTimeLeft] = useState(10);
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
   function startTimer() {
-    setInterval(() => {
+    if (intervalRef.current != null) return;
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
         return 0;
       });
     }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef === null) return;
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+    intervalRef.current = null;
+  }
+
+  function resetTimer() {
+    setTimeLeft(25 * 60);
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+    intervalRef.current = null;
   }
 
   const minutes = padTime(Math.floor(timeLeft / 60));
@@ -32,9 +51,9 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
